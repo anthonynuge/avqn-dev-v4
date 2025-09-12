d701196637b56fc60e139dccecce745c0eac5990
antho <anthonynuge0509@gmail.com>
2025-09-11 19:09:08 -0500

    Import gsap dev tools for debugging.

--- src/lib/gsapSetup.js
+++ src/lib/gsapSetup.js
@@ -2,7 +2,10 @@ import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import ScrambleTextPlugin from 'gsap/ScrambleTextPlugin'
 import ScrollTrigger from 'gsap/ScrollTrigger'
+import { GSDevTools } from 'gsap/GSDevTools'
 
-gsap.registerPlugin(ScrambleTextPlugin, ScrollTrigger)
+gsap.registerPlugin(ScrambleTextPlugin, ScrollTrigger, GSDevTools)
+
+GSDevTools.create()
 
 export { useGSAP, gsap }
