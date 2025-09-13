20dbeced1b512461582265a2cb018c7bd42da580
antho <anthonynuge0509@gmail.com>
2025-09-12 23:15:15 -0500

    Add svg icon.

---
+++ src/assets/ArrowRight.jsx
@@ -0,0 +1,20 @@
+export default function ArrowRight({ className = '', strokeWidth = '2', ...props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      {...props}
+    >
+      <path d="M5 12h14" />
+      <path d="m12 5 7 7-7 7" />
+    </svg>
+  )
+}
