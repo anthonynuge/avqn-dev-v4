c5a7c269ae2568d7aa9825f5745607afa29bebbd
antho <anthonynuge0509@gmail.com>
2025-09-16 15:29:15 -0500

    Add gradient border for absolute text.

--- src/components/CarouselView.jsx
+++ src/components/CarouselView.jsx
@@ -22,6 +22,14 @@ export default function CarouselView({
     <div className={`relative overflow-hidden ${heightClass} ${className}`}>
       {/* Fill the frame; ParallaxImage is absolute/inset-0 internally */}
       <ParallaxImage src={src} alt={`${altPrefix} ${safeIndex + 1}`} shift={shift} />
+
+      {/* Gradient overlay */}
+      <div className="pointer-events-none absolute inset-0">
+        {/* top gradient */}
+        <div className="absolute top-0 right-0 left-0 h-24 bg-gradient-to-b from-black/50 to-transparent" />
+        {/* bottom gradient */}
+        <div className="absolute right-0 bottom-0 left-0 h-24 bg-gradient-to-t from-black/50 to-transparent" />
+      </div>
     </div>
   )
 }
