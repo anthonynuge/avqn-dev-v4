7f4549a35fb9ec2bd04f67c84699c2195d87f80c
antho <anthonynuge0509@gmail.com>
2025-09-26 18:05:28 -0500

    Remove lenis because unused.

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import { RouterProvider } from 'react-router'
-import { ReactLenis } from 'lenis/react'
 import { router } from './router'
 
 import '@/styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ReactLenis root>
-      <RouterProvider router={router} />
-    </ReactLenis>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
