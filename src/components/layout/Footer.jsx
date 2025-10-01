ae0b52e0698dc3b7a047283f60a948544b05fe25
antho <anthonynuge0509@gmail.com>
2025-10-01 18:13:27 -0500

    Add separate padding for mobile.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="inner fixed bottom-0 flex h-[var(--footer-h)] items-center justify-between pt-2 pb-2 text-xs">
+    <footer className="inner fixed bottom-0 flex h-[var(--footer-h)] items-center justify-between pt-1 pb-[1px] text-xs lg:pt-2 lg:pb-2">
       <span className="font-mono">© {new Date().getFullYear()} AVQN.dev</span>
       <span className="font-mono">V4.0</span>
     </footer>
